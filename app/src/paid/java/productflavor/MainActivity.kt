dee6aa37f9f0221a51240b9d4c72e7bc5c1a080d
Bhargav Rasesh Patel <A00303747@canadahanson.ca>
2024-05-29 14:35:04 -0400

    Initial commit

---
+++ app/src/paid/java/productflavor/MainActivity.kt
@@ -0,0 +1,12 @@
+package productflavor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import hanson.bhargav.app1.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
